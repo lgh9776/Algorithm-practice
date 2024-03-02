0eb36c44706223a76b67fef61f66f71952170de0
202010958 이가현 <57521555+lgh9776@users.noreply.github.com>
2024-03-02 23:08:49 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void selectionSort(int a[], int c)
+{
+	int min = a[0];
+	int minIdx = 0;
+
+	for (int i = 0; i < c;) {
+		for (int j = i + 1; j < c; j++) {
+			if (min > a[j]) {
+				min = a[j];
+				minIdx = j;
+			}
+		}
+		a[minIdx] = a[i];
+		a[i] = min;
+
+		min = a[++i];
+		minIdx = i;
+	}
+}
+
+int main()
+{
+	int cnt;
+	int num[1000];
+
+	scanf("%d", &cnt);
+	for (int i = 0; i < cnt; i++)
+		scanf("%d", &num[i]);
+
+	selectionSort(num, cnt);
+
+	for (int i = 0; i < cnt; i++)
+		printf("%d ", num[i]);
+}
