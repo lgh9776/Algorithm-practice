f58548d98a25645f3c20d5f95b9b4b57f3a6015f
이가현 <lgh9776@naver.com>
2024-08-01 20:58:35 +0900

    [D3] Title: 수의 새로운 연산, Time: 230 ms, Memory: 36,364 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.java
@@ -1,15 +1,11 @@
 import java.util.*;
 
 /*
- * p, q의 최대 입력값 = 10000
- * p + q의 최댓값 = 20000
- * 142*142 배열에 다 넣을 수 있음
- * 
  * 1. 값 -> 좌표
  * 대각선 하단의 값은 1 + 2 + 3 + ... + n
  * 이 값이 찾으려는 수보다 커질 경우, 해당 대각선 라인 탐색
  * 해당 대각선 라인은 하단의 값보다 1씩 작아짐
- * -> 찾으려는 값과 같아지면 좌표 찾는 것
+ * 찾으려는 값과 같아지면 좌표 찾는 것
  * 
  * 2. 좌표 -> 값
  * (7,4)를 찾는다는 가정
@@ -34,7 +30,7 @@ class Solution
 			int px = -1, py = -1, qx = -1, qy = -1;
 			int pn = -1, qn = -1;
 			
-            //px, qx값 찾기
+            //px, qx값 찾기 -> 해당하는 대각선을 찾기 위해서 x좌표만 찾음
 			int num = 0; //계속 증가하는 하단값 계산
 			int index = 1;
 			while(true) {
@@ -54,7 +50,7 @@ class Solution
 				index++;
 			}
 			
-			//(px, 1)를 끝으로 하는 대각선 탐색 -> p찾기
+			//(px, 1)를 끝으로 하는 대각선 탐색하여 찾기
 			row = px; //해당 대각선만 확인하면 돼서 반복 필요X
 			col = 1;
 			num = pn;
@@ -69,27 +65,26 @@ class Solution
 				num--; //대각선 거슬러 올라갈수록 --
 			}
 			
-			//(qx, 1)를 끝으로 하는 대각선 탐색 -> q찾기
+			//(qx, 1)를 끝으로 하는 대각선 탐색하여 q찾기
 			row = qx;
 			col = 1;
 			num = qn;
-			for(index = 1; index <= qx; index++) { //대각선 n번째의 요소 수는 n이므로 qx번 반복
+			for(index = 1; index <= qx; index++) { 
 				if(q == num) {
-					qx = row; //진짜 px, py
+					qx = row;
 					qy = col;
 					break;
 				}
 				row--;
 				col++;
-				num--; //대각선 거슬러 올라갈수록 --
+				num--;
 			}
 			
-			//p값과 q값을 가지는 좌표 찾음
 			//별 연산
 			int starX = px + qx;
 			int starY = py + qy;
 			
-			//별 연산으로 얻은 좌표에 해당하는 값
+			//별 연산으로 얻은 좌표 x, y값
 			//먼저 (starX, 1)의 값 찾기
 			int starNumX = 0;
 			for(index = 1; index <= starX; index++) {
@@ -98,7 +93,7 @@ class Solution
 			
 			//(starX, 1)의 값을 이용해서(starX, starY) 좌표의 값 찾기
 			//위로 올라갈수록 순차적으로 커지는 x좌표값을 (starX, 1)에더해주면 됨
-			//starX = x좌표값
+			//starX = 별연산 후 x좌표값
 			//starNumX = (starX, 1)에 해당하는 값
 			//index = 순차적으로 증가시켜주기 위한 값
 			int starNum = starNumX; //별 연산으로 나온 좌표가 가리키는 곳의 값, 시작하는 곳(starX, 1)의 값 셋팅
