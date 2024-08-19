d0a540114774102d78b1418e64fa9f20c18f8ab2
이가현 <lgh9776@naver.com>
2024-08-19 17:33:09 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 105 ms, Memory: 18,620 KB -BaekjoonHub

--- SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
@@ -1,16 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
+/*
+ * 1. 알파벳 입력 받기 (Alphabet)
+ * 2. 아스키코드를 이용해서 숫자 변환 후 출력
+ */
 
-        String abcStr = sc.next();
-        char[] abcArr = abcStr.toCharArray();
-        
-        for(char c : abcArr) {
-        	System.out.print(c - 'A' + 1 + " ");
-        }
+public class Solution {
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		//1. 알파벳 입력 받기 (Alphabet)
+		char[] alphabet = st.nextToken().toCharArray();
+		
+		//2. 아스키코드를 이용해서 숫자 변환 후 출력
+		for(int index = 0; index < alphabet.length; index++) {
+			int wordNum = alphabet[index] - 'A' + 1;
+			sb.append(wordNum).append(" ");
+		}
+		System.out.print(sb);
 	}
 }
