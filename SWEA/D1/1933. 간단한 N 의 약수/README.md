862937ea9b3bb7d3f00e7118d434bbcc267d4cd1
이가현 <lgh9776@naver.com>
2024-08-19 21:17:38 +0900

    [D1] Title: 간단한 N 의 약수, Time: 106 ms, Memory: 18,444 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/README.md
@@ -0,0 +1,15 @@
+# [D1] 간단한 N 의 약수 - 1933 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PhcWaAKIDFAUq) 
+
+### 성능 요약
+
+메모리: 18,444 KB, 시간: 106 ms, 코드길이: 755 Bytes
+
+### 제출 일자
+
+2024-08-19 21:17
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 1. 정수 1개 (num) 입력 받기
+ * 2. n의 약수 구하기
+ */
+
+public class Solution {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		//1. 숫자 2개 (firstNum, secondNum) 입력 받기
+		int num = Integer.parseInt(st.nextToken());
+		
+		//2. n의 약수 구하기
+		for(int count = 1; count <= num; count++) {
+			if(num % count == 0) {
+				sb.append(count).append(" ");
+			}
+		}
+		System.out.print(sb);
+	}
+}
