0bdad98ffdf807881f4052774317f817b236e0d1
이가현 <lgh9776@naver.com>
2024-08-20 22:43:27 +0900

    [D2] Title: 지그재그 숫자, Time: 107 ms, Memory: 19,076 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -1,25 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+/*
+ * 1. 테스트 케이스 입력 받기
+ * 2. 숫자 1개 (num) 입력 받기
+ * 3. 1~num까지의 숫자 중 
+ * 	3-1. 홀수는 더하기
+ * 	3-2. 짝수는 빼기 
+ */
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int n = sc.nextInt();
+public class Solution {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		//1. 테스트 케이스 입력 받기
+		int test_case = Integer.parseInt(st.nextToken());
+		
+		for(int testCase = 1; testCase <= test_case; testCase++) {
+			sb.append("#").append(testCase).append(" ");
 			
-			if(n % 2 == 0) {
-				n = (n / 2) * -1;
-			}
-			else {
-				n = (n+1)/2;
-			}
+			//2. 숫자 1개 (num) 입력 받기
+			st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
 			
-			System.out.printf("#%d %d\n", test_case, n);
+			//3. 1~num까지의 숫자 중
+			int sum = 0;
+			for(int count = 1; count <= num; count++) {
+				//3-1. 홀수는 더하기
+				if(count % 2 == 1) {
+					sum += count;
+				}
+				//3-2. 짝수는 빼기 
+				else if(count % 2 == 0) {
+					sum -= count;
+				}
+			}
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
 	}
 }
