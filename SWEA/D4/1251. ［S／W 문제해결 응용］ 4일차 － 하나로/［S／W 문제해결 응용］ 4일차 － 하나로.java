7bf79da5312642deb7a02bc9cb39ed1b6c246733
이가현 <lgh9776@naver.com>
2024-09-01 18:28:34 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 713 ms, Memory: 65,800 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -5,61 +5,52 @@ import java.util.*;
 
 /*
  * 1. 테스트 케이스 입력 받기
- * 2. 섬의 개수 입력 받기
- * 3. 섬의 row좌표들 입력 받기
- * 4. 섬의 col좌표들 입력 받기
- * 5. 환경 부담 세율 실수 입력 받기
- * 6. 각 정점에서 갈 수 있는 간선 정보 가중치와 함께 리스트에 추가
- * 7. 가중치가 작은 간선부터 오름차순 정렬
- * 8. 각 섬들을 최소 단위 집합으로 초기화 (make-set)
- * 9. 정렬된 간선을 차례대로 꺼내오기
- * 10. union으로 간선의 양 끝 정점이 같은 집합이 아니면 start에 합치기 
- * 	10-1. 총합 환경 부담금에 더하기
- * 11. 같은 집합이면 pass
- * 12. parents에 모두 같은 부모를 가지면 return
- * 13. 총합 환경 부담금 반올림해서 출력 
+ * 2. 섬의 개수 입력 받기 (vCnt)
+ * 3. 섬의 정보 입력 받기 (좌표)
+ * 4. 환경 부담 세율 입력 받기
+ * 5. 아무 섬 우선순위 큐에 추가하기
+ * 6. 공백 큐가 될 때까지 반복
+ * 	6-1. 큐에서 섬 꺼내기 (가중치가 가장 낮은 것 꺼내짐)
+ * 	6-2. 선택한 섬에 방문한적 없으면
+ * 	6-3. 해당 섬 방문 처리
+ * 	6-4. 총 세금에 해당 섬까지의 세금 더하기
+ * 	6-5. 선택된 섬에서 방문하지 않은 다른 섬으로 갈 수 있게 우선순위 큐에 넣기
+ * 
  */
 
 public class Solution {
 	static BufferedReader br;
-	static StringTokenizer st;
 	static StringBuilder sb;
+	static StringTokenizer st;
 	
-	static class Island{
-		int x;
-		int y;
+	static class Locate{
+		int locate_x, locate_y;
 		
-		public Island(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
+		public Locate(int x) {
+			locate_x = x;
 		}
 	}
 	
 	static class Edge implements Comparable<Edge>{
-		int start;
-		int end;
-		double distanceTax;
-		
-		public Edge(int start, int end, double distanceTax) {
-			this.start = start;
-			this.end = end;
-			this.distanceTax = distanceTax;
+		int vertex;
+		double weight;
+
+		public Edge(int vertex, double weight) {
+			this.vertex = vertex;
+			this.weight = weight;
 		}
 
 		@Override
 		public int compareTo(Edge o) {
-			//가중치로 정렬할 수 있게
-			return Double.compare(this.distanceTax, o.distanceTax);
+			return Double.compare(this.weight, o.weight);
 		}
 	}
 	
-	static int islandCnt;
-	static Island[] islands;
-	static Edge[] islandEdge;
+	static int vCnt;
 	static double tax;
-	static double totalTax;
-	static int[] parents;
+	static Locate[] islands;
+	static boolean[] isVisited;
+	static double minTotalTax;
 	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -68,100 +59,66 @@ public class Solution {
 		//1. 테스트 케이스 입력 받기
 		st = new StringTokenizer(br.readLine().trim());
 		int test_case = Integer.parseInt(st.nextToken());
-		for (int testCase = 1; testCase <= test_case; testCase++) {
-			sb.append("#").append(testCase).append(" ");
-			inputData();
-			
-			//간선 정보 저장
-			edgeData();
-			
-			//8. 각 섬들을 최소 단위 집합으로 초기화 (make-set)
-			makeSet();
-			
-			//9. 정렬된 간선을 차례대로 꺼내오기
-			for (int cnt = 0; cnt < islandEdge.length; cnt++) {
-				//10. union으로 간선의 양 끝 정점이 같은 집합이 아니면 start에 합치기 
-				if(union(islandEdge[cnt].start, islandEdge[cnt].end)) {
-					//10-1. 총합 환경 부담금에 더하기 
-					totalTax += islandEdge[cnt].distanceTax;
-				}
-				
-				//11. 같은 집합이면 pass
-				//12. parents에 모두 같은 부모를 가지면 return
-			}
-			
-			//13. 총합 환경 부담금 반올림해서 출력 
-			sb.append(Math.round(totalTax)).append("\n");
+		for (int tc = 1; tc <= test_case; tc++) {
+			sb.append("#").append(tc).append(" ");
+			inputDate();
+			findMinTax();
+			sb.append(Math.round(minTotalTax)).append("\n");
 		}
 		System.out.print(sb);
 	}
 	
-	static void inputData() throws IOException {
-		//2. 섬의 개수 입력 받기
+	static void inputDate() throws IOException {
+		//2. 섬의 개수 입력 받기 (vCnt)
 		st = new StringTokenizer(br.readLine().trim());
-		islandCnt = Integer.parseInt(st.nextToken());
-		parents = new int[islandCnt];
-		totalTax = 0;
+		vCnt = Integer.parseInt(st.nextToken());
+		isVisited = new boolean[vCnt];
+		minTotalTax = 0;
 		
-		//3. 섬의 x좌표들 입력 받기
-		islands = new Island[islandCnt];
+		//3. 섬의 정보 입력 받기 (좌표)
+		islands = new Locate[vCnt];
 		st = new StringTokenizer(br.readLine().trim());
-		for (int index = 0; index < islandCnt; index++) {
-			islands[index] = new Island(Integer.parseInt(st.nextToken()), 0);
+		for (int index = 0; index < vCnt; index++) { //x좌표
+			islands[index] = new Locate(Integer.parseInt(st.nextToken()));
 		}
 		
-		//4. 섬의 y좌표들 입력 받기
 		st = new StringTokenizer(br.readLine().trim());
-		for (int index = 0; index < islandCnt; index++) {
-			islands[index].y = Integer.parseInt(st.nextToken());
+		for (int index = 0; index < vCnt; index++) { //y좌표
+			islands[index].locate_y = Integer.parseInt(st.nextToken());
 		}
 		
-		//5. 환경 부담 세율 실수 입력 받기
+		//4. 환경 부담 세율 입력 받기
 		st = new StringTokenizer(br.readLine().trim());
 		tax = Double.parseDouble(st.nextToken());
 	}
-
-	static void edgeData() {
-		//6. 각 정점에서 갈 수 있는 간선 정보 가중치와 함께 리스트에 추가
-		islandEdge = new Edge[islandCnt*(islandCnt-1)/2];
-		int cnt = 0;
-		for(int start = 0; start < islandCnt-1; start++) {
-			for(int end = start+1; end < islandCnt; end++) {
-				//해저터널 길이 계산 
-				long calDistance = (long) (Math.pow(islands[start].x-islands[end].x, 2) + Math.pow(islands[start].y-islands[end].y, 2));
-				//환경 부담금
-				double calDistanceTax = tax * calDistance;
-				islandEdge[cnt] = new Edge(start, end, calDistanceTax);
-				cnt++;
+	
+	static void findMinTax() {
+		//5. 아무 섬 우선순위 큐에 추가하기
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.offer(new Edge(0, 0));
+		
+		//6. 공백 큐가 될 때까지 반복
+		while(!pq.isEmpty()) {
+			//6-1. 큐에서 섬 꺼내기 (가중치가 가장 낮은 것 꺼내짐)
+			Edge pickIsland = pq.poll();
+			
+			//6-2. 선택한 섬에 방문한적 없으면
+			if(!isVisited[pickIsland.vertex]) {
+				//6-3. 해당 섬 방문 처리
+				isVisited[pickIsland.vertex] = true;
+				
+				//6-4. 총 세금에 해당 섬까지의 세금 더하기
+				minTotalTax += pickIsland.weight;
+				
+				//6-5. 선택된 섬에서 방문하지 않은 다른 섬으로 갈 수 있게 우선순위 큐에 넣기
+				for (int index = 0; index < vCnt; index++) {
+					if(!isVisited[index]) {
+						double xLen =  Math.pow(islands[pickIsland.vertex].locate_x - islands[index].locate_x, 2);
+						double yLen =  Math.pow(islands[pickIsland.vertex].locate_y - islands[index].locate_y, 2);
+						pq.offer(new Edge(index, (xLen + yLen) * tax));	
+					}
+				}	
 			}
 		}
-		
-		//7. 가중치가 작은 간선부터 오름차순 정렬
-		Arrays.sort(islandEdge);
-	}
-	
-	static void makeSet() {
-		for (int cnt = 0; cnt < islandCnt; cnt++) {
-			parents[cnt] = -1;	
-		}
-	}
-	
-	//요소가 속한 집합의 루트 반환
-	static int findSet(int element) {
-		if(parents[element] < 0)
-			return element;
-		return parents[element] = findSet(parents[element]);
-	}
-	
-	static boolean union(int first, int second) {
-		int firstRoot = findSet(first);
-		int secondRoot = findSet(second);
-		
-		if(firstRoot == secondRoot)
-			return false;
-		
-		parents[firstRoot] += parents[secondRoot];
-		parents[secondRoot] = firstRoot;
-		return true;
 	}
 }
