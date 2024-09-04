410b3108fa12f960f5e07c67878a8b5d6aa80b65
이가현 <lgh9776@naver.com>
2024-09-04 20:36:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 108 ms, Memory: 18,192 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
  * 1. 테스트 케이스 입력 받기
- * 2. 이용권 가격 입력 받기
- * 3. 월별 이용 계획 입력 받기
- * 4. 인자로 들어온 월이 12를 넘어가면 return (백트래킹)
- * 5. 현재 요금이 min보다 클 경우 return (백트래킹)
- * 6. 12와 같을 때 min요금 갱신 후 return (끝까지 찾은 경우)
- * 7. 각 월에 대해서 1일, 1개월, 3개월, 1년 요금 순으로 계산
- * 	7-1. 시작하는 월, 이용권에 따라 이번 달+a의 요금을 인자로 재귀 호출
+ * 2. 각 이용권 요금 입력 	받기
+ * 3. 각 월의 이용계획 입력 받기
+ * 4. 12월까지 항상 해당 월까지 최적의 값을 구해주기
+ * 	4-1. 3월 전까지 1일, 1개월 이용권 고려
+ * 	4-2. 3월부터 3개월 이용권까지 고려
+ * 5. 12월달에 저장된 값은 12월을 고려한 값 -> 1년 이용권과 비교
  */
 
 public class Solution {
@@ -19,67 +19,60 @@ public class Solution {
 	static StringTokenizer st;
 	static StringBuilder sb;
 	
-	static int minCharge;
-	static int[] ticketCharge = new int[4];
-	static int[] plan = new int[12];
+	static int oneDayPay;
+	static int oneMonthPay;
+	static int threeMonthPay;
+	static int oneYearPay;
 	
+	static int[] plan;
+	static int[] charge;
+			
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine().trim());
-		sb = new StringBuilder();		
+		sb = new StringBuilder();
 		
 		//1. 테스트 케이스 입력 받기
+		st = new StringTokenizer(br.readLine().trim());
 		int test_case = Integer.parseInt(st.nextToken());
-		for(int testCase = 1; testCase <= test_case; testCase++) {
-			sb.append("#").append(testCase).append(" ");
+		for (int tc = 1; tc <= test_case; tc++) {
+			sb.append("#").append(tc).append(" ");
 			
 			inputData();
-			calCharge(1, 0);
-			
-			sb.append(minCharge).append("\n");
+
+			//4. 12월까지 항상 해당 월까지 최적의 값을 구해주기
+			for (int month = 1; month <= 12; month++) {
+				//4-1. 3월 전까지 1일, 1개월 이용권 고려  + 이전 달까지의 최적값
+				if(month < 3) {
+					charge[month] = Math.min(plan[month] * oneDayPay, oneMonthPay) + charge[month-1];
+				}
+				//4-2. 3월부터 3개월 이용권까지 고려 
+				//1일, 1개월 이용권은 이전 달까지 최적합 더해주기
+				//3개월 이용권은 3달 전까지의 최적합 더해주기
+				else {
+					charge[month] = Math.min(plan[month] * oneDayPay, oneMonthPay) + charge[month-1];
+					charge[month] = Math.min(charge[month], threeMonthPay + charge[month-3]);
+				}
+			}
+			//5. 12월달에 저장된 값은 12월을 고려한 값 -> 1년 이용권과 비교 후 min 출력
+			sb.append(Math.min(charge[12], oneYearPay)).append("\n");
 		}
 		System.out.print(sb);
 	}
 	
 	static void inputData() throws IOException {
-		minCharge = Integer.MAX_VALUE;
-		
-		//2. 이용권 가격 입력 받기
+		//2. 각 이용권 요금 입력 받기
 		st = new StringTokenizer(br.readLine().trim());
-		for(int ticket = 0; ticket < 4; ticket++) {
-			ticketCharge[ticket] = Integer.parseInt(st.nextToken());
-		}
+		oneDayPay = Integer.parseInt(st.nextToken());
+		oneMonthPay = Integer.parseInt(st.nextToken());
+		threeMonthPay = Integer.parseInt(st.nextToken());
+		oneYearPay = Integer.parseInt(st.nextToken());
 		
-		//3. 월별 이용 계획 입력 받기
+		//3. 각 월의 이용계획 입력 받기
+		plan = new int[13];
 		st = new StringTokenizer(br.readLine().trim());
-		for(int month = 0; month < 12; month++) {
+		for (int month = 1; month <= 12; month++) {
 			plan[month] = Integer.parseInt(st.nextToken());
 		}
-	}
-	
-	//인자로 들어오는 월이 현재 처리할 월
-	static void calCharge(int month, int currentCharge) {
-		//4. 인자로 들어온 월이 12를 넘어가면 return (백트래킹)
-		if(month > 13) {
-			return;
-		}
-		
-		//5. 현재 요금이 min보다 크거나 같은 경우 return (백트래킹)
-		if(currentCharge >= minCharge) {
-			return;
-		}
-		
-		//6. 13와 같을 때 (12월까지 처리했다는 뜻) min요금 갱신 후 return (끝까지 찾은 경우)
-		if(month == 13) {
-			minCharge = Math.min(minCharge, currentCharge);
-			return;
-		}
-		
-		//7. 각 월에 대해서 1일, 1개월, 3개월, 1년 요금 순으로 계산
-		//7-1. 시작하는 월, 이용권에 따라 이번 달+a의 요금을 인자로 재귀 호출
-		calCharge(month+1, currentCharge + ticketCharge[0] * plan[month-1]);
-		calCharge(month+1, currentCharge + ticketCharge[1]);
-		calCharge(month+3, currentCharge + ticketCharge[2]);
-		calCharge(month+12, currentCharge + ticketCharge[3]);
+		charge = new int[13];
 	}
 }
