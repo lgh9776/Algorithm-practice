bb7230726167d2a737e70f46d4c81db3386360e0
이가현 <lgh9776@naver.com>
2025-03-08 08:54:44 +0900

    [Silver II] Title: 좌표 압축, Time: 2116 ms, Memory: 288808 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static int size;
+	static int[] originArr;
+	static List<Integer> zipArr;
+	
+	public static void main(String[] args) throws IOException {
+		
+		inputData();
+		
+		int index = 0;
+		for (int i = 0; i < size; i++) {
+			index = binarySearch(originArr[i]);
+			if(index == -1) {
+				continue;
+			}
+			sb.append(index).append(" ");
+		}
+		System.out.println(sb);
+	}
+	
+	static void inputData() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine());
+		size = Integer.parseInt(st.nextToken());
+		
+		originArr = new int[size];
+		HashSet<Integer> temp = new HashSet<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < size; i++) {
+			originArr[i] = Integer.parseInt(st.nextToken());
+			temp.add(originArr[i]);
+		}
+		
+		zipArr = new ArrayList<Integer>();
+		for(int num : temp) {
+			zipArr.add(num);
+		}
+		Collections.sort(zipArr);
+	}
+	
+	static int binarySearch(int target) {
+		int start = 0;
+		int end = zipArr.size()-1;
+		
+		int mid = 0;
+		while(start <= end) {
+			mid = start + (end - start) / 2;
+			
+			if(zipArr.get(mid) == target) {
+				return mid;
+			}
+			else if(zipArr.get(mid) > target) {
+				end = mid - 1;
+			}
+			else if(zipArr.get(mid) < target) {
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+}
