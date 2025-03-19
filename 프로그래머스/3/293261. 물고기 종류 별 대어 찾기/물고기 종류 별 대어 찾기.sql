00b410bd34ae90bb8c30b952c7b90e95dba86845
이가현 <lgh9776@naver.com>
2025-03-19 10:27:25 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,18 +1,15 @@
-WITH max_len_info AS (
-    SELECT fish_type, MAX(length) AS max_length
-    FROM fish_info
-    WHERE length > 10 -- 10cm 이하 제외
-    GROUP BY fish_type
-),
-ranked_fish AS (
-    SELECT fi.id, fi.fish_type, fi.length,
-           ROW_NUMBER() OVER (PARTITION BY fi.fish_type ORDER BY fi.id) AS rn
-    FROM fish_info fi
-    JOIN max_len_info mli
-    ON fi.fish_type = mli.fish_type AND fi.length = mli.max_length
-)
-SELECT r.id AS ID, fni.fish_name AS FISH_NAME, r.length AS LENGTH
-FROM ranked_fish r
-JOIN fish_name_info fni ON r.fish_type = fni.fish_type
-WHERE r.rn = 1 -- 가장 큰 물고기 중 ID가 가장 작은 1마리만 선택
-ORDER BY r.id;
+WITH fish_max_len AS (
+SELECT fish_info.fish_type, max(length) as len
+FROM fish_info
+GROUP BY fish_info.fish_type),
+
+fish_max_with_id AS (
+SELECT id, fml.fish_type, fml.len
+FROM fish_info fi JOIN fish_max_len fml
+ON fi.fish_type = fml.fish_type
+AND fi.length = fml.len
+ORDER BY id )
+
+SELECT id AS ID, fish_name AS FISH_NAME, fmwi.len AS LENGTH
+FROM fish_max_with_id fmwi JOIN fish_name_info fni
+ON fmwi.fish_type = fni.fish_type
