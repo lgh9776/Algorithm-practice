3a5e7c4372a36673261f16acf2e5c1c97f9e2fe7
이가현 <lgh9776@naver.com>
2026-01-06 01:34:16 +0900

    [Silver IV] Title: 큐 2, Time: 1056 ms, Memory: 325324 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		ArrayDeque<Integer> q = new ArrayDeque<Integer>();
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			
+			if(s.equals("push")) {
+				int num = Integer.parseInt(st.nextToken());
+				q.offer(num);
+			}
+			else if(s.equals("pop")) {
+				if(q.isEmpty()) {
+					sb.append("-1").append("\n");
+				}
+				else {
+					sb.append(q.poll()).append("\n");
+				}
+			}
+			else if(s.equals("size")) {
+				sb.append(q.size()).append("\n");
+			}
+			else if(s.equals("empty")) {
+				if(q.isEmpty()) {
+					sb.append("1").append("\n");
+				}
+				else {
+					sb.append("0").append("\n");
+				}
+			}
+			else if(s.equals("front")) {
+				if(q.isEmpty()) {
+					sb.append("-1").append("\n");
+				}
+				else {
+					sb.append(q.peekFirst()).append("\n");
+				}
+			}
+			else if(s.equals("back")) {
+				if(q.isEmpty()) {
+					sb.append("-1").append("\n");
+				}
+				else {
+					sb.append(q.peekLast()).append("\n");
+				}
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
